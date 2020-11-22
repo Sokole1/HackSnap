9f8aa2e2f495d9c9992f0cf0a00730690392af5a
unknown <jason2580134@outlook.com>
2020-11-21 19:37:59 -0800

    Driver

--- scrape.py
+++ scrape.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 
-def scrape_group():
-    pass
+# def scrape_group():
+#     pass
 
-def scrape_subject():
-    pass 
+# def scrape_subject():
+#     pass 
 
-def scrape_yearmonth():
-    pass
+# def scrape_yearmonth():
+#     pass
+
+driver = webdriver.Chrome("C:\Users\YI's PC\Desktop\HackSnap\chromedriver.exe")
+

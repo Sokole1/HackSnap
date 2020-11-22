5b4ead0c80bcfce1457b44711ce976b809625400
WilliamKang <kaangwill@gmail.com>
2020-11-21 22:13:41 -0800

    Webdrive

--- scrape.py
+++ scrape.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
 import sys
-import time 
-
+import time
 ### Set up the webdriver
 def resource_path(relative_path):
     try:
@@ -10,6 +12,23 @@ def resource_path(relative_path):
     except Exception:
         base_path = os.path.dirname(__file__)
     return os.path.join(base_path, relative_path)
+
+def scrape_group(choice):
+
+    # xpaths is a list of the xpaths for each of the 6 groups
+    driver.implicitly_wait(10)
+    xpaths = ['//*[@id="fileList"]/tr[1]/td[1]/a/span[1]/span', '//*[@id="fileList"]/tr[2]/td[1]/a/span[1]/span', '//*[@id="fileList"]/tr[3]/td[1]/a/span[1]/span', '//*[@id="fileList"]/tr[4]/td[1]/a/span[1]/span', '//*[@id="fileList"]/tr[5]/td[1]/a/span[1]/span', '//*[@id="fileList"]/tr[6]/td[1]/a/span[1]/span']
+    for xpath in xpaths:
+        elem = driver.find_element_by_xpath(xpath)
+        if elem.text == choice:
+            elem.click()
+    
+
+def scrape_subject():
+    pass 
+
+def scrape_yearmonth():
+    pass
 
 driver = webdriver.Chrome(resource_path('./chromedriver.exe'))
 ###
@@ -20,13 +39,8 @@ driver.find_element_by_id("password").send_keys("examsnap")
 input = driver.find_element_by_id("password-submit")
 input.click()
 
-def scrape_group():
-    pass
 
-def scrape_subject():
-    pass 
+scrape_group("Group 4 - Sciences")
 
-def scrape_yearmonth():
-    pass
 
 

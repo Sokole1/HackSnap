d9a86b732ab0ea67e40feabb6019e0fabac0eacc
iExisted <hanaakira@yahoo.com>
2020-11-22 00:04:15 -0800

    html and js changes

---
+++ templates/HackSnap.js
@@ -0,0 +1,29 @@
+var groupValue = document.getElementById("groupDropDown");
+var subjectDropArea = document.getElementById("subjectArea");
+
+//Check the subject group selected
+function checkSubjGroup(){
+    console.log(document.getElementById("groupDropDown").value);
+    switch(document.getElementById("groupDropDown").value){
+        case "Group 1 - Studies in Language and Literature":
+            document.getElementById("subjectDropDown").innerHTML="<option value='English'>English</option>"
+            break;
+        case "Group 2 - Language Acquisition":
+            document.getElementById("subjectDropDown").innerHTML="<option value='spanish'>Spanish</option><option value='french'>French</option><option value='mandarin'>Mandarin</option><option value='japanese'>Japanese</option>"
+            break;
+        case "Group 3 - Individuals and Societies":
+            document.getElementById("subjectDropDown").innerHTML="<option value='buisness'>Buisness</option><option value='itgs'>ITGS</option><option value='geography'>Geography</option><option value='history'>History</option>"
+            break;
+        case "Group 4 - Experimental Sciences":
+            document.getElementById("subjectDropDown").innerHTML="<option value='physics'>Physics</option><option value='biology'>Biology</option><option value='chemistry'>Chemistry</option>"
+            break;
+        case "Group 5 - Mathematics":
+            document.getElementById("subjectDropDown").innerHTML="<option value='math'>Math</option>"
+            break;
+        case "Group 6 - The Arts":
+            document.getElementById("subjectDropDown").innerHTML="<option value='music'>Music</option>"
+            break;
+        default:
+            console.log("no Option")
+    }
+}
